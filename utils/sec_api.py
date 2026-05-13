cad2cca6da59f115277e21f1004040a011b8fe33
TraderFs69 <fsanscartier@hotmail.com>
2026-05-13 17:43:52 -0400

    Update sec_api.py

--- utils/sec_api.py
+++ utils/sec_api.py
@@ -4,9 +4,21 @@ HEADERS = {
     "User-Agent": "TradingEnAction fsanscartier@hotmail.com"
 }
 
-def get_company_facts(cik):
+def get_cik_from_ticker(ticker):
 
-    cik = str(cik).zfill(10)
+    url = "https://www.sec.gov/files/company_tickers.json"
+
+    data = requests.get(url, headers=HEADERS).json()
+
+    for company in data.values():
+
+        if company['ticker'] == ticker.upper():
+            return str(company['cik_str']).zfill(10)
+
+    return None
+
+
+def get_company_facts(cik):
 
     url = f"https://data.sec.gov/api/xbrl/companyfacts/CIK{cik}.json"
 
